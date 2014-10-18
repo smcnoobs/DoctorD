401914b1b46fcde76ffde1afb953ed3c6df66327
Samuel <pattch@users.noreply.github.com>
2014-10-18 13:46:20 -0700

    Amir Did This.
    For real, Amir is the coolest guy. We need to get him some congratulatory coke. And lettuce.

---
+++ PlayerWarper.java
@@ -0,0 +1,17 @@
+package doctord;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.geom.Vector2f;
+
+public class PlayerWarper extends Item {
+
+	private static int duration;
+	
+	public PlayerWarper(int dur, Animation sprites, Vector2f location ){
+		
+		super(sprites, location);
+		
+	}
+	
+	
+	
+}
