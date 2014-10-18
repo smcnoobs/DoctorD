673eb293e865a6dfe9fadba4fa49957a9f743f55
Chunp2 <chunpaul000@gmail.com>
2014-10-18 14:24:01 -0700

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -6,9 +6,10 @@ import org.newdawn.slick.geom.Vector2f;
 public class Projectile extends Item {
 	private int damage;
 	
-	public Projectile ( int d, Animation sprites, Vector2F location) {
-		this.damage = d;
+	public Projectile ( int d, Animation sprites, Vector2f location) {
 		super (sprites, location);
+		this.damage = d;
+		
 	}
 	
 	public int getDamage() {
