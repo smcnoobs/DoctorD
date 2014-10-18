89ef78760d96a3cbe71799b1be7032d65097abc8
Samuel <pattch@users.noreply.github.com>
2014-10-18 14:28:37 -0700

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -7,10 +7,6 @@ public class Actor implements Actable {
 	private Animation sprites;
 	private Vector2f location;
 	
-	public Actor() {
-		// TODO Auto-generated constructor stub
-	}
-	
 	public Actor ( Animation spirtes, Vector2f location)
 	{
 		this.sprites = sprites;
