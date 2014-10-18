472df2ddacf731d6252f94787cc718c82d2f4aec
Chunp2 <chunpaul000@gmail.com>
2014-10-18 14:12:47 -0700

    Create PillarBlock.java

---
+++ PillarBlock.java
@@ -0,0 +1,26 @@
+package doctord;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.Animation;
+
+public class PillarBlock extends Pillar{
+	
+	private boolean hidden;
+	
+	public boolean isHidden()
+	{
+	  return true;	
+	}
+	public void setSprites()
+	{
+		
+	}
+	public PillarBlock ( Animation sprites, Vector2f location)
+	{
+		
+	}
+	public PillarBlock() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
